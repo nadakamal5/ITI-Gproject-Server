33ce56626b0b11c6ef1fd31e4e1ec8f54f5286aa
Nada kamal <120226503+nadakamal5@users.noreply.github.com>
2023-03-08 21:20:25 +0200

    Add files via upload

---
+++ Kalaks_backend/index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+//express app
+const app = express();
+
+const usersRouter = require("./routes/user");
+const adminRouter = require("./routes/admin");
+const carsShopRouter = require("./routes/cars_shop");
+const carsnewRouter = require("./routes/carsnew");
+const carsusedRouter = require("./routes/carsused");
+const schoolsRouter = require("./routes/schools");
+const maintainRouter = require("./routes/maintain");
+const accessShopRouter = require("./routes/accessories_shop");
+const accessRouter = require("./routes/accessories");
+const cartRouter = require("./routes/cart");
+
+//middleware
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("images"));
+
+//routes
+app.use("/users", usersRouter);
+app.use("/admins", adminRouter);
+app.use("/carsshops", carsShopRouter);
+app.use("/newcars", carsnewRouter);
+app.use("/usedcars", carsusedRouter);
+app.use("/schools", schoolsRouter);
+app.use("/maintains", maintainRouter);
+app.use("/accessShops", accessShopRouter);
+app.use("/accessories", accessRouter);
+app.use("/cart", cartRouter);
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcom to the app" });
+});
+
+//database connection
+mongoose.connect("mongodb://127.0.0.1:27017/gpproject", (err) => {
+  if (!err) return console.log("DB Connected");
+  console.log(err);
+});
+
+//listen for requsts
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port`, process.env.PORT);
+});
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+
+app.post('/payments', async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+
+    res.status(200).send(paymentIntent.client_secret);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// app.post("/payment", async (req, res) => {
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment",
+//       line_items: req.body.items.map((item) => {
+//         return {
+//           price_data: {
+//             currency: "usd",
+//             product_data: {
+//               name: item.name,
+//             },
+//             unit_amount: item.price,
+//           },
+//           quantity: item.quantity,
+//         };
+//       }),
+//       success_url: `http://localhost:3000/`,
+//       // cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+//     });
+//     res.json({ url: session.url });
+//     // res.json({ url: session.url });
+//   } catch (e) {
+//     res.status(500).json({ error: e.message });
+//   }
+// });
+
